d48b80155bf0594014947a7caf30d92270726115
Emil Stenberg <esandin@gmail.com>
2024-05-17 22:56:53 +0200

    print 2d array of samples

--- scripts/export_fluid_surface_to_3d_samples.py
+++ scripts/export_fluid_surface_to_3d_samples.py
@@ -8,15 +8,17 @@ output_filepath = os.path.join(output_directory, output_filename)
 file = open(output_filepath, "w")
 result = []
 for x in range(100):
-    ray_begin = Vector((8, 0+x, 111))
-    ray_end = Vector((8, 0+x, -100))
-    ray_begin_local = target_object.matrix_world.inverted() @ ray_begin
-    ray_direction = ray_end - ray_begin
-    ray_direction.normalize()
-    hit, location, normals, index = target_object.ray_cast(ray_begin_local, ray_direction)
-    print(hit, location, normals, index)
-    sample = location.z
-    result.append(sample)
+    row = []
+    for y in range(100):
+        ray_begin = Vector((0+x, 0+y, 111))
+        ray_end = Vector((0+x, 0+y, -100))
+        ray_begin_local = target_object.matrix_world.inverted() @ ray_begin
+        ray_direction = ray_end - ray_begin
+        ray_direction.normalize()
+        hit, location, normals, index = target_object.ray_cast(ray_begin_local, ray_direction)
+        sample = location.z
+        row.append(sample)
+    result.append(row)
 
 file.write(json.dumps(result))
 file.close()

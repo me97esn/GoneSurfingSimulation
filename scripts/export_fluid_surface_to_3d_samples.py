dc2625fb0f64d6bd44d0e72a231e314b7a92d15f
Emil Stenberg <esandin@gmail.com>
2024-05-19 10:08:25 +0200

    even fewer samples

--- scripts/export_fluid_surface_to_3d_samples.py
+++ scripts/export_fluid_surface_to_3d_samples.py
@@ -15,11 +15,11 @@ result = []
 for frame in range(start_frame, end_frame+1):
     scn.frame_set(frame)
     frame_samples = []
-    for x in range(30):
+    for x in range(10):
         row = []
-        for y in range(30):
-            ray_begin = Vector((0+3*x, 0+3*y, 111))
-            ray_end = Vector((0+3*x, 0+3*y, -100))
+        for y in range(10):
+            ray_begin = Vector((0+10*x, 0+10*y, 111))
+            ray_end = Vector((0+10*x, 0+10*y, -100))
             ray_begin_local = target_object.matrix_world.inverted() @ ray_begin
             ray_direction = ray_end - ray_begin
             ray_direction.normalize()

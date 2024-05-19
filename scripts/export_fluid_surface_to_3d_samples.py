520b773f6b44957935ca51035974ed5faabd15dd
Emil Stenberg <esandin@gmail.com>
2024-05-19 13:00:34 +0200

    adjust the sample script to include entire ocean

--- scripts/export_fluid_surface_to_3d_samples.py
+++ scripts/export_fluid_surface_to_3d_samples.py
@@ -15,11 +15,14 @@ result = []
 for frame in range(start_frame, end_frame+1):
     scn.frame_set(frame)
     frame_samples = []
-    for x in range(10):
+    step = 3
+    y_length = 400
+    x_length = 150
+    for x in range(x_length/step):
         row = []
-        for y in range(10):
-            ray_begin = Vector((0+10*x, 0+10*y, 111))
-            ray_end = Vector((0+10*x, 0+10*y, -100))
+        for y in range(y_length/step):
+            ray_begin = Vector((-70+step * x, -250+step*y, 111))
+            ray_end = Vector((-70+step*x, -250+step*y, -100))
             ray_begin_local = target_object.matrix_world.inverted() @ ray_begin
             ray_direction = ray_end - ray_begin
             ray_direction.normalize()

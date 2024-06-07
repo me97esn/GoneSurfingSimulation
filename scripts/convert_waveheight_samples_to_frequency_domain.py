76628b02ba943f1bdb265c995538878ad1cc8ea1
Emil Stenberg <esandin@gmail.com>
2024-06-07 09:31:48 +0200

    wip: creating a script to convert samples to time domain

--- scripts/convert_waveheight_samples_to_frequency_domain.py
+++ scripts/convert_waveheight_samples_to_frequency_domain.py
@@ -1,11 +1,15 @@
 import numpy as np
 import json
+import os
 
-f = open('wave_samples.json')
+directory = "/home/emil/workspace/GoneSurfingScripts"
+filename = "wave_samples.json"
+filename = os.path.join(directory, filename)
+f = open(filename, "r")
 samples = json.load(f)
 
 output_filename_freqs = "wave_frequencies_subset.json"
-output_filepath_freqs = os.path.join(output_directory, output_filename_freqs)
+output_filepath_freqs = os.path.join(directory, output_filename_freqs)
 file_freqs = open(output_filepath_freqs, "w")
 number_of_frequencies_to_include = 10
 frequencies_result = {"number_of_frequencies_to_include": number_of_frequencies_to_include }
@@ -13,7 +17,7 @@ frequencies_result = {"number_of_frequencies_to_include": number_of_frequencies_
 # This is the number of low frequency frequencies, and high frequency frequencies to include. They are by coincidence the same number.
 number_of_freqs = 15
 frequencies_all_frames = [np.fft.fftn(frame) for frame in samples]
-filtered_frequencies_all_frames = [np.array([[z for zi, z in enumerate(arr) if zi < number_of_freqs or zi >= len(arr)-number_of_freqs] for arr in frame_frequencies]).toList() for frame_frequencies in frequencies_all_frames]
+filtered_frequencies_all_frames = [np.array([[z for zi, z in enumerate(arr) if zi < number_of_freqs or zi >= len(arr)-number_of_freqs] for arr in frame_frequencies]) for frame_frequencies in frequencies_all_frames]
 frequencies_result["frequencies_per_frame"] = filtered_frequencies_all_frames
 file_freqs.write(json.dumps(frequencies_result))
 file_freqs.close()

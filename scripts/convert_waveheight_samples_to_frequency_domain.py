1e3b0aeb92e00b97b99c88f369545beeaa985794
Emil Stenberg <esandin@gmail.com>
2024-06-08 00:42:06 +0200

    adjusted the freqs to include

--- scripts/convert_waveheight_samples_to_frequency_domain.py
+++ scripts/convert_waveheight_samples_to_frequency_domain.py
@@ -24,9 +24,9 @@ number_of_frequencies_to_include = 10
 frequencies_result = {"number_of_frequencies_to_include": number_of_frequencies_to_include, "len_x": len(samples[0]), "len_y": len(samples[0][0])}
 
 # This is the number of low frequency frequencies, and high frequency frequencies to include. They are by coincidence the same number.
-number_of_freqs = 30
+number_of_freqs = 30 
 frequencies_all_frames = [np.fft.fftn(frame) for frame in samples]
-filtered_frequencies_all_frames = [np.array([[z for zi, z in enumerate(arr) if zi < number_of_freqs or zi >= len(arr)-number_of_freqs] for arr in frame_frequencies]) for frame_frequencies in frequencies_all_frames]
+filtered_frequencies_all_frames = [np.array([[z for zi, z in enumerate(arr) if zi < number_of_freqs or zi >= len(arr)-number_of_freqs ] for arr in frame_frequencies]) for frame_frequencies in frequencies_all_frames]
 frequencies_result["frequencies_per_frame"] = filtered_frequencies_all_frames
 file_freqs.write(json.dumps(frequencies_result, cls=NumpyArrayEncoder))
 file_freqs.close()

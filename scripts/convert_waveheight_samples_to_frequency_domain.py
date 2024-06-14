bce9453aa5fd089eba5ad1935fe408fe88b20efb
Emil Stenberg <esandin@gmail.com>
2024-06-14 11:16:07 +0200

    include step_size when converting to freq domain

--- scripts/convert_waveheight_samples_to_frequency_domain.py
+++ scripts/convert_waveheight_samples_to_frequency_domain.py
@@ -17,11 +17,13 @@ number_of_rows_to_include = int(sys.argv[4]) or 10
 
 filename = sys.argv[1] 
 f = open(filename, "r")
-samples = json.load(f)
+samples_data = json.load(f)
+samples = samples_data["samples"]
 
 output_filepath_freqs = sys.argv[2] 
 file_freqs = open(output_filepath_freqs, "w")
 frequencies_result = {
+    "step_size": samples_data["step_size"],
     "number_of_frequencies_to_include": number_of_frequencies_to_include,
     "number_of_rows_to_include": number_of_rows_to_include,
     "len_x": len(samples[0]),

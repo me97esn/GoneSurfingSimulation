e64d401fc4570b8127234377544952c609348568
Emil Stenberg <esandin@gmail.com>
2024-07-02 15:59:55 +0200

    loop thru the source data

--- scripts/convert_frequencies_json_to_ue4_datatable_format.js
+++ scripts/convert_frequencies_json_to_ue4_datatable_format.js
@@ -4,4 +4,16 @@ const content = fs.readFileSync(sourceFileURL, {
   encoding: "utf8",
   flag: "r",
 });
-const data = JSON.parse(content);
+const sourceData = JSON.parse(content);
+console.log(sourceData.frequencies_per_frame.length);
+const result = [];
+let i = 0;
+for (let frequency2darray in sourceData.frequencies_per_frame) {
+  result.push({
+    Name: `Frame_${i}`,
+    step_size: sourceData.step_size,
+  });
+  i++;
+}
+
+console.log(result);

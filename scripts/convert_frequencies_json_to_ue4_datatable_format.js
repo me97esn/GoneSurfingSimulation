e7c1c5fc71e2812cb02c9469b7bc6d69a55042c1
Emil Stenberg <esandin@gmail.com>
2024-07-02 16:01:23 +0200

    create first level of struct

--- scripts/convert_frequencies_json_to_ue4_datatable_format.js
+++ scripts/convert_frequencies_json_to_ue4_datatable_format.js
@@ -12,6 +12,11 @@ for (let frequency2darray in sourceData.frequencies_per_frame) {
   result.push({
     Name: `Frame_${i}`,
     step_size: sourceData.step_size,
+    number_of_frequencies_to_include:
+      sourceData.number_of_frequencies_to_include,
+    number_of_rows_to_include: sourceData.number_of_rows_to_include,
+    len_x: sourceData.len_x,
+    len_y: sourceData.len_y,
   });
   i++;
 }

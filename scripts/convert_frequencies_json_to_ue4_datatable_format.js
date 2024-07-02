a07fc173ff61a2c766e834faa48c598382c7c95d
Emil Stenberg <esandin@gmail.com>
2024-07-02 16:09:14 +0200

    wip: writing frequencies, but too few

--- scripts/convert_frequencies_json_to_ue4_datatable_format.js
+++ scripts/convert_frequencies_json_to_ue4_datatable_format.js
@@ -8,7 +8,9 @@ const sourceData = JSON.parse(content);
 console.log(sourceData.frequencies_per_frame.length);
 const result = [];
 let i = 0;
+// TODO: should the frames start with 0 or 1?
 for (let frequency2darray in sourceData.frequencies_per_frame) {
+  const frequencies = [];
   result.push({
     Name: `Frame_${i}`,
     step_size: sourceData.step_size,
@@ -17,8 +19,15 @@ for (let frequency2darray in sourceData.frequencies_per_frame) {
     number_of_rows_to_include: sourceData.number_of_rows_to_include,
     len_x: sourceData.len_x,
     len_y: sourceData.len_y,
+    frequencies_this_frame: frequencies,
   });
+  for (let row in frequency2darray) {
+    const encapsulatingObj = { arr: [] };
+    encapsulatingObj.arr.push(row);
+    frequencies.push(encapsulatingObj);
+  }
+
   i++;
 }
 
-console.log(result);
+console.log(JSON.stringify(result, null, 2));

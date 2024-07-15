8dc6bc9ebf4209869e966f64a78d5277ef668fd4
Emil Stenberg <esandin@gmail.com>
2024-07-15 23:11:05 +0200

    write meta data to an array of length 1

--- scripts/convert_frequencies_json_to_ue4_datatable_format.js
+++ scripts/convert_frequencies_json_to_ue4_datatable_format.js
@@ -10,15 +10,19 @@ let i = 0;
 function trimFloat(float, num_of_decimals = 2) {
   return parseFloat(float.toFixed(num_of_decimals));
 }
-const metadata = {
-  step_size: sourceData.step_size,
-  number_of_frequencies_to_include: sourceData.number_of_frequencies_to_include,
-  number_of_rows_to_include: sourceData.number_of_rows_to_include,
-  start_trace_x: sourceData.start_trace_x,
-  start_trace_y: sourceData.start_trace_y,
-  len_x: sourceData.len_x,
-  len_y: sourceData.len_y,
-};
+const metadata = [
+  {
+    Name: "Metadata",
+    step_size: sourceData.step_size,
+    number_of_frequencies_to_include:
+      sourceData.number_of_frequencies_to_include,
+    number_of_rows_to_include: sourceData.number_of_rows_to_include,
+    start_trace_x: sourceData.start_trace_x,
+    start_trace_y: sourceData.start_trace_y,
+    len_x: sourceData.len_x,
+    len_y: sourceData.len_y,
+  },
+];
 
 for (let frequency2darray of sourceData.frequencies_per_frame) {
   const frequencies = [];

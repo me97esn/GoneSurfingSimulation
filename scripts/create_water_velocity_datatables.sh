d690aa3f93da6e296f265aff0a23270808827a36
Emil Stenberg <esandin@gmail.com>
2024-08-02 22:06:21 +0200

    write files for y and z as well

--- scripts/create_water_velocity_datatables.sh
+++ scripts/create_water_velocity_datatables.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 start_frame=752
-end_frame=760 #1325
+end_frame=1325
 node -max-old-space-size=32768 create_water_velocity_datatable.js /tmp/medium_wave_left_water_velocities $start_frame $end_frame  
 python3 convert_vertices_to_samples.py /tmp/medium_wave_left_water_velocities /tmp/medium_wave_left_water_velocities_samples $start_frame $end_frame
+
 python3 convert_waveheight_samples_to_frequency_domain.py /tmp/medium_wave_left_water_velocities_samples/x_samples.json /tmp/medium_wave_left_water_velocities_samples/x_frequencies.json 25 120
+python3 convert_waveheight_samples_to_frequency_domain.py /tmp/medium_wave_left_water_velocities_samples/y_samples.json /tmp/medium_wave_left_water_velocities_samples/y_frequencies.json 25 120
+python3 convert_waveheight_samples_to_frequency_domain.py /tmp/medium_wave_left_water_velocities_samples/z_samples.json /tmp/medium_wave_left_water_velocities_samples/z_frequencies.json 25 120
 
 node convert_frequencies_json_to_ue4_datatable_format.js /tmp/medium_wave_left_water_velocities_samples/x_frequencies.json /tmp/velocity_x_frequencies_struct.json /tmp/velocity_x_frequencies_struct_metadata.json
+node convert_frequencies_json_to_ue4_datatable_format.js /tmp/medium_wave_left_water_velocities_samples/y_frequencies.json /tmp/velocity_y_frequencies_struct.json /tmp/velocity_y_frequencies_struct_metadata.json
+node convert_frequencies_json_to_ue4_datatable_format.js /tmp/medium_wave_left_water_velocities_samples/z_frequencies.json /tmp/velocity_z_frequencies_struct.json /tmp/velocity_z_frequencies_struct_metadata.json

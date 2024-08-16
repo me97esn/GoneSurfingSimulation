5706a6ff0ccdbc1578e2c0b5ff384c260ce754ba
Emil Stenberg <esandin@gmail.com>
2024-08-17 00:19:29 +0200

    removed code for writing two files in one script

--- scripts/convert_vertices_to_samples.py
+++ scripts/convert_vertices_to_samples.py
@@ -24,6 +24,14 @@ start_frame = int(sys.argv[3])
 end_frame = int(sys.argv[4])
 values = sys.argv[5]
 
+smallest_x = None
+step_size = 0.5 
+minValue = 3
+samples_dict_per_frame = {}
+samples_dict_per_frame= {}
+x_border=20
+y_border=40
+
 onlyfiles = [f for f in listdir(source_folder) if isfile(join(source_folder, f))]
 
 def filterSamplesAboveMinValue(coordinates, values, minValue):
@@ -35,13 +43,6 @@ def filterSamplesAboveMinValue(coordinates, values, minValue):
             filtered_values.append(value)
     return filtered_coordinates, filtered_values
 
-smallest_x = None
-step_size = 0.5 
-minValue = 3
-samples_dict_per_frame = {}
-non_reformatted_samples_dict_per_frame = {}
-x_border=20
-y_border=60
 # y_samples_dict_per_frame = {}
 # z_samples_dict_per_frame = {}
 for file in onlyfiles:
@@ -98,44 +99,13 @@ grid_y: [[0.   0.25 0.5  0.75 1.  ]
         """
         return [[grid_x[i][j], grid_y[i][j]] for i in range(len(grid_x)) for j in range(len(grid_x[0]))]
 
-    def reformat_data(samples_grid):
-        return samples_grid.tolist()
-        # samples_per_x_coord = {}
-        # for x in sample_x_coords:
-        #     samples_per_x_coord[x] = []
-        # for i, sample in enumerate(samples_grid):
-        #     coord = sample_coords[i]
-        #     samples_per_x_coord[coord[0]].append(sample)
-        # reformatted_result = []
-        # for x in samples_per_x_coord:
-        #     reformatted_result.append(samples_per_x_coord[x])
-        # return reformatted_result
-        # Restructure the data to use the same format as the wave height data
+    array_of_values = convert_grid_to_array_of_values(grid_samples)
+    array_of_coordinates = convert_grid_to_coordinates(grid_x, grid_y)
 
-    # samples_dict_per_frame[int(file.replace('.json', ''))] = reformat_data(grid_x)
-    # non_reformatted_samples_dict_per_frame[int(file.replace('.json', ''))] = {"samples":data[values], "coordinates":data['coordinates']}
+    samples_dict_per_frame[int(file.replace('.json', ''))] = {"samples":array_of_values, "coordinates":array_of_coordinates}
 
-    non_reformatted_samples_dict_per_frame[int(file.replace('.json', ''))] = {"samples":convert_grid_to_array_of_values(grid_samples), "coordinates":convert_grid_to_coordinates(grid_x, grid_y)}
-    # non_reformatted_samples_dict_per_frame[int(file.replace('.json', ''))] = {"samples":grid_x, "coordinates": sample_coords}
-
-    # y_samples_dict_per_frame[int(file.replace('.json', ''))] = reformat_data(grid_y)
-    # z_samples_dict_per_frame[int(file.replace('.json', ''))] = reformat_data(grid_z)
-
-def format_samples_to_ue4_struct_format(data_dict):
-    output_samples = []
-    for frame in range(start_frame, end_frame+1):
-        if frame not in data_dict:
-            print(f"Frame {frame} not found in data_dict")
-            continue
-        output_samples.append(data_dict[frame])
-     
-    return {"step_size": step_size, "samples": output_samples, "start_frame": start_frame, "end_frame": end_frame, "start_trace_x": smallest_x, "start_trace_y": smallest_y, "x_length": int( largest_x - smallest_x ), "y_length": int( largest_y - smallest_y ) }
-# print('writing to', target_file)
-# file = open(target_file, "w")
-# file.write(json.dumps(format_samples_to_ue4_struct_format(samples_dict_per_frame)))
-target_file_non_formatted = f"{target_file.replace('.json', '')}_non_formatted.json"
-file_non_formatted = open(target_file_non_formatted, "w")
-print('writing to', target_file_non_formatted)
-file_non_formatted.write(json.dumps(non_reformatted_samples_dict_per_frame, cls=NumpyArrayEncoder))
-# file.close()
+target_file = f"{target_file}"
+f = open(target_file , "w")
+print('writing to', target_file )
+f.write(json.dumps(samples_dict_per_frame, cls=NumpyArrayEncoder))
 

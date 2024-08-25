760bfa0b48f88d81eb42dc458ede9c7c57442df9
Emil Stenberg <esandin@gmail.com>
2024-08-25 16:18:45 +0200

    sample all frames, but with step size: 5

--- scripts/export_fluid_surface_to_3d_samples.py
+++ scripts/export_fluid_surface_to_3d_samples.py
@@ -2,10 +2,10 @@ import os
 import json
 target_object = bpy.data.objects['fluid_surface']
 start_frame = 752
-# end_frame = 1325
-end_frame = 754
+end_frame = 1325
+# end_frame = 754
 scn = bpy.context.scene
-step = 0.5
+step = 5 
 output_directory = "/home/emil/workspace/GoneSurfingScripts"
 output_filename = "wave_samples.json"
 output_filepath = os.path.join(output_directory, output_filename)

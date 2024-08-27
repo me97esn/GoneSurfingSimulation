e55b331a1693f16bf366311ec304a011afeec3c3
Emil Stenberg <esandin@gmail.com>
2024-08-27 10:10:28 +0200

    export one file for samples, and three separate files for each normals axis

--- scripts/export_fluid_surface_to_3d_samples.py
+++ scripts/export_fluid_surface_to_3d_samples.py
@@ -2,14 +2,15 @@ import os
 import json
 target_object = bpy.data.objects['fluid_surface']
 start_frame = 752
-end_frame = 1325
-# end_frame = 754
+# end_frame = 1325
+end_frame = 754
 scn = bpy.context.scene
 step = 5 
 output_directory = "/home/emil/workspace/GoneSurfingScripts"
 output_filename = "wave_samples.json"
-output_filepath = os.path.join(output_directory, output_filename)
-file = open(output_filepath, "w")
+output_filename_normals_x = "wave_normals_x.json"
+output_filename_normals_y = "wave_normals_y.json"
+output_filename_normals_z = "wave_normals_z.json"
 samples = {
     "step_size":step, 
     "samples":[], 
@@ -19,17 +20,25 @@ samples = {
     "start_trace_y":-200, 
     "x_length":160, 
     "y_length":350, 
-    "coordinates":[],
-    "normals":[]}
+    "coordinates":[]}
+# Create copies of the samples, for storing normals in the same format as the samples
+normals_x = samples.copy()
+normals_y = samples.copy()
+normals_z = samples.copy()
+
 for frame in range(start_frame, end_frame+1):
     scn.frame_set(frame)
     frame_samples = []
     frame_coordinates = []
-    frame_normals = []
+    frame_normals_x = []
+    frame_normals_y = []
+    frame_normals_z = []
     for x in range(int(samples["x_length"]/step)):
         row = []
         coordinates_row = []
-        normals_row = []
+        normals_row_x = []
+        normals_row_y = []
+        normals_row_z = []
         for y in range(int(samples["y_length"]/step)):
             ray_begin = Vector((samples["start_trace_x"]+step * x, samples["start_trace_y"]+step*y, 100))
             ray_end = Vector((samples["start_trace_x"]+step*x, samples["start_trace_y"]+step*y, -100))
@@ -40,13 +49,42 @@ for frame in range(start_frame, end_frame+1):
             sample = location.z
             row.append(sample)
             coordinates_row.append([location.x, location.y])
-            normals_row.append([normals.x, normals.y, normals.z])
+            normals_row_x.append(normals.x)
+            normals_row_y.append(normals.y)
+            normals_row_z.append(normals.z)
         frame_samples.append(row)
         frame_coordinates.append(coordinates_row)
-        frame_normals.append(normals_row)
-    samples["samples"].append(frame_samples)
-    samples["coordinates"].append(frame_coordinates)
-    samples["normals"].append(frame_normals)
+        frame_normals_x.append(normals_row_x)
+        frame_normals_y.append(normals_row_y)
+        frame_normals_z.append(normals_row_z)
 
+
+    for d in [normals_x, normals_y, normals_z, samples]:
+        d["coordinates"].append(frame_coordinates)
+
+    samples["samples"].append(frame_samples)
+    normals_x["samples"].append(frame_normals_x)
+    normals_y["samples"].append(frame_normals_y)
+    normals_z["samples"].append(frame_normals_z)
+
+output_filepath = os.path.join(output_directory, output_filename)
+file = open(output_filepath, "w")
 file.write(json.dumps(samples))
 file.close()
+
+output_filepath_normals_x = os.path.join(output_directory, output_filename_normals_x)
+file = open(output_filepath_normals_x, "w")
+file.write(json.dumps(normals_x))
+file.close()
+
+output_filepath_normals_y = os.path.join(output_directory, output_filename_normals_y)
+file = open(output_filepath_normals_y, "w")
+file.write(json.dumps(normals_y))
+file.close()
+
+output_filepath_normals_z = os.path.join(output_directory, output_filename_normals_z)
+file = open(output_filepath_normals_z, "w")
+file.write(json.dumps(normals_z))
+file.close()
+
+print('written sampling files')

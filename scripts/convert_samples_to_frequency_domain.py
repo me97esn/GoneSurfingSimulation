d4f4918473ae0ec352a8dc1c498d33ba50bc9618
Emil Stenberg <esandin@gmail.com>
2024-08-27 08:15:15 +0200

    added comment about normals

--- scripts/convert_samples_to_frequency_domain.py
+++ scripts/convert_samples_to_frequency_domain.py
@@ -16,24 +16,32 @@ filename = sys.argv[1]
 f = open(filename, "r")
 
 ## Expects data on the format:
-# {
-#     "start_trace_y": 0,
-#     "start_trace_x": 0,
-#     "step_size": 1,
-#     "start_frame": 0,
-#     "samples": [ # frame
-#         [ # row
-#             [0.1, 0.2, 0.3],# column
-#             [0.4, 0.5, 0.6],# column
-#             [0.7, 0.8, 0.9]# column
-#         ], # row
-#         [
-#             [0.1, 0.2, 0.3],# column
-#             [0.4, 0.5, 0.6],# column
-#             [0.7, 0.8, 0.9]# column
-#         ]
-#     ]
-# }
+"""
+{
+    "start_trace_y": 0,
+    "start_trace_x": 0,
+    "step_size": 1,
+    "start_frame": 0,
+    "samples": [ # frame
+        [ # row
+            [0.1, 0.2, 0.3],# column
+            [0.4, 0.5, 0.6],# column
+            [0.7, 0.8, 0.9]# column
+        ], # row
+        [
+            [0.1, 0.2, 0.3],# column
+            [0.4, 0.5, 0.6],# column
+            [0.7, 0.8, 0.9]# column
+        ]
+    ]
+    "normals": [# frame
+        [ # row
+            [#column
+                [0,0,1] #Vector3d, [0,0,0.32], [0,0,1]],...
+        ], 
+
+}
+"""
 # Note that no coordinates is in the data, this is because samples are evenly spread with step_size from start_trace_x and start_trace_y
 
 

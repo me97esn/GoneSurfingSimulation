cbb1f9280962320f694d71467244882edd80d25c
Emil Stenberg <esandin@gmail.com>
2025-11-07 17:00:40 +0100

    made the ocean a lot wider

--- scripts/open_simulation.sh
+++ scripts/open_simulation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/home/emil/blender-2.83/blender-2.83.17-stable+v283.430262b133a4-linux.x86_64-release/blender --python open_simulation.py 
+ /snap/bin/blender --python open_simulation.py 
 # blender --python open_simulation.py 
